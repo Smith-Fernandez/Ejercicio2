78954f9eb6360c1aed6e25dfb1f1c610f6564835
Smith-Fernandez <smith18200@gmail.com>
2021-12-03 17:52:10 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio2/Ejercicio2/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Item1.Clicked += async (sender,e) =>
+            {
+                await Navigation.PushAsync(new Page1());
+            };
+            Item2.Clicked += async (sender, e) => { await Navigation.PushAsync(new TabbedPage1()); };
+            Item3.Clicked += async (sender, e) => { await Navigation.PushAsync(new CarouselPage1()); };
+            Item4.Clicked += async (sender, e) => { /*para llamar al popup await */ await Navigation.PushModalAsync(new ModalPage1()); };
+            Item5.Clicked += async (sender, e) =>
+            {/*par llamar un popup de tipo confirm*/
+                var answer = await DisplayAlert("Pregunta", "te gustaria jugar", "Si", "No");
+                Debug.WriteLine("Answer: " + answer);
+            };
+        }
+    }
+}
